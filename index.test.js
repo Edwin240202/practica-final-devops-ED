7cd9f0a017334aedeefd8cc9bc1b16ea14804140
Edwin2402.github.io <20211902@itla.edu.do>
2023-12-14 19:28:21 -0400

    test prueba completa

--- index.test.js
+++ index.test.js
@@ -1,7 +1,10 @@
-// index.test.js
+const { JSDOM } = require('jsdom');
+
 describe('Test Hola Mundo', () => {
-    test('should pass', () => {
-      expect(true).toBe(true);
-    });
+  test('should pass', () => {
+    const dom = new JSDOM('<!DOCTYPE html><html lang="es"><body><h1>Hola Mundo</h1></body></html>');
+    global.document = dom.window.document;
+
+    expect(document.body.textContent).toContain('Hola Mundo');
   });
-  
+});
